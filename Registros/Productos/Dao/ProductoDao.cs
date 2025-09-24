22f249751f3f2a618f06c6eeabae4b6976b3b4a7
Jeremy Gradiz <jjgradiz@uamv.edu.ni>
2025-09-24 11:22:23 -0600

    Add files via upload

---
+++ Registros/Productos/Dao/ProductoDao.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Productos.Modelos;
+
+namespace Productos.Dao
+{
+    public class ProductoDao
+    {
+        
+        private Producto[] carrito = new Producto[10];
+        private int pos = 0;
+        public void Agregar(Producto prod)
+        {
+            try
+            {
+
+            
+            carrito[pos++] = prod;
+            }catch(IndexOutOfRangeException)
+            {
+                MessageBox.Show("Tu mama ya anda en bolas por tu culpa", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        public Producto[] VerCarrito()
+
+        {
+            return carrito;
+        }
+
+    }
+}
